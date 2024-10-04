4444aa09d75979a5081036fc313f4a067d8759e5
Ivan Barsukov <i.barsukov@protonmail.com>
2024-10-04 17:00:13 +0700

    Format code

--- favicon/util.py
+++ favicon/util.py
@@ -5,16 +5,19 @@ That module provides two function for extracting favicons:
 
 Technical details about favicon: http://en.wikipedia.org/wiki/Favicon
 """
+
 from urllib.request import urlopen
 import re
 from urllib.parse import urljoin, urlsplit
 import logging
 import socket
 
-__all__ = ['find_favicon_from_url', 'find_favicon_from_dump']
+__all__ = ["find_favicon_from_url", "find_favicon_from_dump"]
 
-RE_LINK = re.compile(r'<link[^>]+>', re.I | re.S)
-RE_ICON_REL = re.compile(r'rel*=\s*["\']?\s*(?:shortcut\s+)?icon[ "\'>]', re.I | re.S)
+RE_LINK = re.compile(r"<link[^>]+>", re.I | re.S)
+RE_ICON_REL = re.compile(
+    r'rel*=\s*["\']?\s*(?:shortcut\s+)?icon[ "\'>]', re.I | re.S
+)
 RE_HREF = re.compile(r'href\s*=\s*["\']?([^"\' >]+)', re.I | re.S)
 SOCKET_TIMEOUT = 30
 
@@ -54,15 +57,15 @@ def find_favicon_from_url(url):
             return icon_url
 
     host = urlsplit(url).hostname
-    icon_url = 'http://%s/favicon.ico' % host
+    icon_url = "http://%s/favicon.ico" % host
     try:
         req = urlopen(icon_url, None, 1)
     except Exception as ex:
         logging.error(ex)
     else:
         if req.code == 200:
-            ctype = req.headers.get('content-type', '')
-            if ctype.startswith('image/'):
+            ctype = req.headers.get("content-type", "")
+            if ctype.startswith("image/"):
                 return icon_url
 
     return None
@@ -100,15 +103,23 @@ def find_favicon_from_dump(dump, page_url=None):
     return icon_url
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     assert find_icon_element('<link rel="icon" href="foo">')
-    assert find_icon_element('<link rel=\'icon\' href="foo">')
+    assert find_icon_element("<link rel='icon' href=\"foo\">")
     assert find_icon_element('<link rel=icon href="foo">')
     assert find_icon_element('<link rel="shortcut icon" href="foo">')
     assert find_icon_element('<link rel="shortcut icon " href="foo">')
 
-    assert find_favicon_from_dump('<link rel="icon" href="foo">') == 'foo'
-    assert find_favicon_from_dump('<link rel="icon" href="foo">', 'http://ya.ru') == 'http://ya.ru/foo'
-    assert find_favicon_from_dump('<link rel="icon" href="/foo">', 'http://ya.ru/bar') == 'http://ya.ru/foo'
+    assert find_favicon_from_dump('<link rel="icon" href="foo">') == "foo"
+    assert (
+        find_favicon_from_dump('<link rel="icon" href="foo">', "http://ya.ru")
+        == "http://ya.ru/foo"
+    )
+    assert (
+        find_favicon_from_dump(
+            '<link rel="icon" href="/foo">', "http://ya.ru/bar"
+        )
+        == "http://ya.ru/foo"
+    )
     # print find_favicon_from_url('http://ya.ru')
     # print find_favicon_from_url('http://bitbucket.org')
